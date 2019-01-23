a69ee2f5b58d6e8d62388b62edf1a7c8d6a8297f
Denver Stove <dtstove@gmail.com>
2019-01-23 11:21:05 +1100

    Made base templates and proper html files for home and about pages. Also linked bootstrap for future use

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+  {
+    'author':'Denver T',
+    'title':"Blodding Around1",
+    'content':'Hellow wwolrd',
+    'date_posted':"December 22, 2019"
+  },
+    {
+    'author': 'Andrew P',
+    'title': "Clocking aaaaaRound",
+    'content': 'How to get the perfect time',
+    'date_posted': "December 52, 2229"
+  }
+]
+
 def home(request):
-  return HttpResponse("<h1>Welcome Blog Home</h1>")
+  context = {
+    'posts': posts
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return HttpResponse("<h1>ABOuuuuTE the Blog</h1>")
+  return render(request, 'blog/about.html', {'title':'About'})
